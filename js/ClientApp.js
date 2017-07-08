1eeb63207c898bee97d8b38eacb33c3bf2821862
Alex Webb <alexwebbb@yahoo.com>
2017-07-08 01:27:04 -0500

    2nd component

---
+++ js/ClientApp.js
@@ -0,0 +1,19 @@
+﻿var LilComponent = () => {
+    return React.createElement('div', null,
+        React.createElement('h1', null, 'Hello, this is the smallest component. Look how we reproduce!')
+    );
+}
+
+var MediumComponent = () => {
+    return React.createElement('div', { className: 'greeting' },
+        React.createElement(LilComponent, null),
+        React.createElement(LilComponent, null),
+        React.createElement(LilComponent, null),
+        React.createElement(LilComponent, null)
+    );
+}
+
+ReactDOM.render(
+    React.createElement(MediumComponent),
+    document.getElementById('app')
+);
