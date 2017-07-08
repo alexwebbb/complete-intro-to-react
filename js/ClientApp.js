7a625ff15db45a824b069cd7f09a3d0eeb7b007c
Alex Webb <alexwebbb@yahoo.com>
2017-07-08 02:04:31 -0500

    props

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,17 +1,23 @@
 ﻿var glorp = React.createElement;
 
-var LilComponent = () => {
+// please include a title
+var TitleComponent = (props) => {
     return glorp('div', null,
-        glorp('h1', null, 'we\'re glorping')
+        glorp('h1', null, props.title)
     );
+}
+
+// please include num1 and num2
+var AdditionComponent = (props) => {
+    return glorp('div', { className: 'math' },
+        props.num1 + props.num2
+    )
 }
 
 var MediumComponent = () => {
     return glorp('div', { className: 'greeting' },
-        glorp(LilComponent, null),
-        glorp(LilComponent, null),
-        glorp(LilComponent, null),
-        glorp(LilComponent, null)
+        glorp(TitleComponent, { title: 'Wuthering Heights' }),
+        glorp(AdditionComponent, { num1: 5, num2: 6 }),
     );
 }
 
